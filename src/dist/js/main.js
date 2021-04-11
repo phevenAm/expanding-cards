65f716f34393c10be4bf36ce5b3f01d529695d53
stephenPC <stephenmissah@outlook.com>
2021-04-11 21:28:58 +0100

    first commit - turned nodelist of panels into array

---
+++ src/dist/js/main.js
@@ -0,0 +1,11 @@
+console.log("start");
+
+const panels = document.querySelectorAll("span");
+
+const panelsArr = Array.from(panels);
+console.log(panelsArr);
+
+// panels.addEventListener("click", () => {
+//   alert(" clicked");
+//   return;
+// });
