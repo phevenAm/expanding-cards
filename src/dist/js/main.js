73b1f7511a70c47175f92d701b210cef67241951
stephenPC <stephenmissah@outlook.com>
2021-04-18 13:01:32 +0100

    js added + media queries

--- src/dist/js/main.js
+++ src/dist/js/main.js
@@ -1,11 +1,20 @@
 console.log("start");
 
-const panels = document.querySelectorAll("span");
+const panels = document.querySelectorAll(".panel");
 
 const panelsArr = Array.from(panels);
 console.log(panelsArr);
 
-// panels.addEventListener("click", () => {
-//   alert(" clicked");
-//   return;
-// });
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeClassAll();
+        return (panel.classList.toggle("active"))
+    })
+
+})
+
+
+const removeClassAll = () => {
+    panels.forEach((panel) => panel.classList.remove("active"));
+}
